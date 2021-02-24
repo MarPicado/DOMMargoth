843280b3abf0d2a2dc36693a0ce323d4b7c463b7
Margoth <mloreana@gmail.com>
2021-02-24 03:40:34 +0000

    exercise 08

--- .breathecode/resets/10-Render-on-Click/index.js
+++ .breathecode/resets/10-Render-on-Click/index.js
@@ -1,7 +1,6 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-	button.getElementbyId("#superDuperButton").addEventListener("mouseover" = "Hello World"));
-});
 
-//no pude hacerlo
+
+});
--- .breathecode/resets/11-Add-LI-on-Click/index.js
+++ .breathecode/resets/11-Add-LI-on-Click/index.js
@@ -1,7 +1,5 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-	button.getElementbyId("#superDuperButton").addEventListener("mouseover" = "Hello World"));
-});
 
-//no pude hacerlo
+});
